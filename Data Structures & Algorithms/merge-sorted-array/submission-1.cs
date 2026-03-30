8eadc527f662af0e222171451da7bb28ebec0063
Justin Klass <54290707+JustinKlass@users.noreply.github.com>
2026-03-30 15:09:57 -0400

    Add: merge-sorted-array - submission-1

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-1.cs
@@ -0,0 +1,27 @@
+public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int last = m + n - 1;
+        int i = m - 1;
+        int j = n - 1;
+
+        while(j >= 0)
+        {
+            if(i >= 0 && nums1[i] > nums2[j])
+            {
+                nums1[last] = nums1[i];
+                i -= 1;
+                last -= 1;
+            }
+
+            else
+            {
+                nums1[last] = nums2[j];
+                j -= 1;
+                last -= 1;
+            }
+        }
+    }
+
+}
