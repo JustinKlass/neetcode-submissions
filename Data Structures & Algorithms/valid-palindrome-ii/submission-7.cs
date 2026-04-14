26d181cb1471a66bda7f0b14cc96e476a7eb4752
Justin Klass <54290707+JustinKlass@users.noreply.github.com>
2026-04-14 14:34:23 -0400

    Add: valid-palindrome-ii - submission-7

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-7.cs
@@ -0,0 +1,41 @@
+public class Solution
+{
+    public bool ValidPalindrome(string s)
+    {
+
+        int left = 0;
+        int right = s.Length - 1;
+
+        while(left < right)
+        {
+            if(s[left] != s[right])
+            {
+                return (IsPalindrome(s, left, right - 1) || IsPalindrome(s, left + 1, right));
+            }
+
+            left += 1;
+            right -= 1;
+        }
+
+        return true;
+        
+    }
+
+
+    private bool IsPalindrome(string s, int left, int right)
+    {
+        while(left < right)
+        {
+
+            if(s[left] != s[right])
+            {
+                return false;
+            }
+
+            left += 1;
+            right -= 1;
+        }
+
+        return true;
+    }
+}
