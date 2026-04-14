faa7709fb4f12543dfc0bf2867a80097cda7c4a8
Justin Klass <54290707+JustinKlass@users.noreply.github.com>
2026-04-14 14:38:45 -0400

    Add: merge-strings-alternately - submission-2

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-2.cs
@@ -0,0 +1,27 @@
+public class Solution
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+            int first = 0;
+            int second = 0;
+            string res = "";
+
+            while(first < word1.Length || second < word2.Length)
+            {
+                if(first < word1.Length)
+                {
+                    res += word1[first];
+                    first += 1;
+                }
+
+                if(second < word2.Length)
+                {
+                    res += word2[second];
+                    second += 1;
+                }
+            }
+
+
+            return res;
+    }
+}
