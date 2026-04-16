da14e655413f57142eaf6b7cd38516cedf437f7e
Justin Klass <54290707+JustinKlass@users.noreply.github.com>
2026-04-16 14:36:40 -0400

    Add: merge-sorted-array - submission-7

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-7.cs
@@ -0,0 +1,34 @@
+public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        // Get the last position 
+        int lastPos = nums1.Length - 1;
+
+        // While there are still non zero numbers to be processed in both arrays
+        while(m > 0 && n > 0)
+        {
+            if(nums1[m - 1] > nums2[n - 1])
+            {
+                nums1[lastPos] = nums1[m - 1];
+                m -= 1;
+            }
+
+            else
+            {
+                nums1[lastPos] = nums2[n - 1];
+                n -= 1;
+            }
+
+            lastPos -= 1;
+        }
+
+        while(n > 0)
+        {
+            nums1[lastPos] = nums2[n - 1];
+            n -= 1;
+
+            lastPos -= 1;
+        }
+    }
+}
