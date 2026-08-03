dfab217a115446ac52b4df5aa4b9eea17e297d25
Justin Klass <54290707+JustinKlass@users.noreply.github.com>
2026-08-03 14:10:38 -0400

    Add: anagram-groups - submission-37

---
+++ Data Structures & Algorithms/anagram-groups/submission-37.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs)
+    {
+        unordered_map<string, vector<string>> res;
+
+        for(auto& str : strs)
+        {
+            vector<int> charCount(26, 0);
+
+            for(char c : str)
+            {
+                charCount[c - 'a']++;
+            }
+
+            string key = to_string(charCount[0]);
+            for(int i = 1; i < 26; ++i)
+            {
+                key += ',' + to_string(charCount[i]);
+            }
+
+            res[key].push_back(str);
+
+        }
+
+        vector<vector<string>> result;
+        for(const auto& pair : res)
+        {
+            result.push_back(pair.second);
+        }
+
+        return result;
+    }
+};
